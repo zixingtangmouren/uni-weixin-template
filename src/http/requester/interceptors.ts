6e3ba3167d83c6b24bc4887da6261158face452f
tamgmouren <83946821+zixingtangmouren@users.noreply.github.com>
2021-06-02 17:05:44 +0800

    fix

--- src/http/requester/interceptors.ts
+++ src/http/requester/interceptors.ts
@@ -2,35 +2,36 @@
  * @Author: tangzhicheng
  * @Date: 2021-06-01 11:01:21
  * @LastEditors: tangzhicheng
- * @LastEditTime: 2021-06-02 16:53:26
+ * @LastEditTime: 2021-06-02 17:05:32
  * @Description: 拦截器对象
  */
 
-import { Task, Codes, GeneralResult, ConfigOptions } from './types'
+import { Task, Codes } from './types'
 import { compose } from '@/utils/common'
 
-/**
- * 处理请求之前/响应之后的相关配置
- */
 export default class Interceptor<T = any, M = T> {
   private successTaskQueue: Task<T, M>[] = []
-  private errorTaskQueue: Task<T, Error | GeneralResult>[] = []
+  private errorTaskQueue: Task<T, M>[] = []
   public codes: Codes = {}
 
   constructor(codes: Codes = {}) {
     this.codes = codes
   }
 
-  public use(successTask: Task<T, M>, errorTask?: Task<T, Error | GeneralResult>) {
+  public use(successTask: Task<T, M>, errorTask?: Task<T, M>) {
     this.successTaskQueue.push(successTask)
     errorTask && (this.errorTaskQueue.push(errorTask))
   }
 
   public runWithSuccessTask<V = any>(material: V): V {
-    return compose<Task<T, M>, V>(...this.successTaskQueue)(material)
+    return this.run<Task<T, M>, V>(this.successTaskQueue)(material)
   }
 
-  public runWithErrorTask<V extends { config: ConfigOptions }>(material: V): V {
-    return compose<Task<T, Error | GeneralResult>, V>(...this.errorTaskQueue)(material)
+  public runWithErrorTask<V = any>(material: V): V {
+    return this.run<Task<T, M>, V>(this.errorTaskQueue)(material)
+  }
+
+  private run<T extends Function, V>(taskQueue: T[]) {
+    return compose<T, V>(...taskQueue)
   }
 }
